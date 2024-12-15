55b01ea19dcef9942d50b20ca3a609ad97b46205
shambhusaran <71879374+shambhusaran@users.noreply.github.com>
2024-12-16 01:17:12 +0545

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
         <div className='flex items-center h-full animate-fade-up animate-once animate-duration-1000 animate-delay-300 animate-ease-out'>
         <div className=" container w-[calc(70%)] flex  mx-auto">
         <div className="flex flex-col w-full h-full  justify-center  ">
-                <div className="text-5xl md:text-7xl  font-montserrat font-bold text-white text-center tracking-normal leading-12"> Impacting Lives Through Technology and Innovation</div>
+                <div className="text-5xl md:text-7xl  font-montserrat font-bold text-white text-center tracking-normal leading-12"> Impacting Livess Through Technology and Innovation</div>
                 <p className=" text-white mt-16 md:px-20 text-2xl md:text-3xl tracking-wider  text-center font-satoshi"> At TGL Technologies, we transform your ideas into products that allows you to turn your imagination into reality. We put a strong emphasis on delivering high-quality outcomes on schedule and precisely as required.</p>
             </div>
         </div>
