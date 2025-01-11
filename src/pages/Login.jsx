552e2002fb424a17b13344bbffaec9dfd3fdc00e
Shambhusaran <shambhusaranparajuli@gmail.com>
2025-01-11 23:37:22 +0545

    login page added functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -35,7 +35,7 @@ const Login = () => {
       });
       const data = await response.json(); 
       console.log(data)
-      if(data){
+      if(data.token){
         navigate('/')
       }
       // navigate('/')
