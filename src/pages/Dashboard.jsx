e011eb294ef4e8be4db03a1a1a7e5248c3fd8faa
shambhusaran <71879374+shambhusaran@users.noreply.github.com>
2025-01-13 22:31:54 +0545

    Update Dashboard for cw.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,8 +9,9 @@ const Dashboard = () => {
         <div className='flex items-center h-full animate-fade-up animate-once animate-duration-1000 animate-delay-300 animate-ease-out'>
         <div className=" container w-[calc(70%)] flex  mx-auto">
         <div className="flex flex-col w-full h-full  justify-center  ">
-                <div className="text-5xl md:text-7xl  font-montserrat font-bold text-white text-center tracking-normal leading-12"> Impacting Lives Through Technology and Innovation</div>
-                <p className=" text-white mt-16 md:px-20 text-2xl md:text-3xl tracking-wider  text-center font-satoshi"> At TGL Technologies, we transform your ideas into products that allows you to turn your imagination into reality. We put a strong emphasis on delivering high-quality outcomes on schedule and precisely as required.</p>
+                <div className="text-5xl md:text-7xl  font-montserrat font-bold text-white text-center tracking-normal leading-12 text-red"> Hackedddd !!!!!!!</div>
+            <p className="text-4xl text-red" >Secure Your System Before Its Tooo LATE !!!!!!!</p>
+{/*                 <p className=" text-white mt-16 md:px-20 text-2xl md:text-3xl tracking-wider  text-center font-satoshi"> At TGL Technologies, we transform your ideas into products that allows you to turn your imagination into reality. We put a strong emphasis on delivering high-quality outcomes on schedule and precisely as required.</p> */}
             </div>
         </div>
         </div>
